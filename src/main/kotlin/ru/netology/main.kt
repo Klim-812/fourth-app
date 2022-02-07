57fc3e649550ea024466f005dde8661f5a54e6ab
a.klimov <Klim-812@yandex.ru>
2022-02-07 21:00:08 +0300

    Задача №3 - "Меломан"

--- src/main/kotlin/ru/netology/main.kt
+++ src/main/kotlin/ru/netology/main.kt
@@ -7,15 +7,21 @@ fun main() {
     val percentMeloman = 1.00/100
     val discount = 100
     val itemPrice = 100
-    val itemCount = 120
-    val melomans = true
+    val itemCount = 110
+    val Melomans = true
 
     val totalPrice = itemPrice * itemCount
     val percentDiscount = totalPrice * percent
-    val result = if (totalPrice>discountStart && totalPrice<discountNext) totalPrice - discount
-    else  if (totalPrice>discountNext) totalPrice-percentDiscount
-    else totalPrice
+    val resultDiscount = (totalPrice - discount) * percentMeloman
+    val resultPercentDiscount = (totalPrice - percentDiscount) * percentMeloman
+    val result =
+        if (totalPrice>discountStart && totalPrice<discountNext && Melomans) totalPrice - discount - resultDiscount
+        else if (totalPrice>discountStart && totalPrice<discountNext && !Melomans) totalPrice - discount
+        else  if (totalPrice>discountNext && Melomans) totalPrice - percentDiscount - resultPercentDiscount
+        else if (totalPrice>discountNext && !Melomans) totalPrice - percentDiscount
+        else totalPrice
 
 
     println("Total Price: $result")
+
 }
